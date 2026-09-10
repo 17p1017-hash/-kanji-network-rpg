65e9cccb6fe0e18c612ff858f7e7423e179e5d85
17p1017-hash <17p1017@stu.meisei-u.ac.jp>
2026-09-10 10:31:18 +0900

    Update kingdom.js

--- maps/kingdom.js
+++ maps/kingdom.js
@@ -360,7 +360,7 @@ const MAP_KINGDOM = {
   row: 2,
 
   x: 60,
-  y: 770,
+  y: 790,
 
   width: 258,
   height: 205
@@ -373,7 +373,7 @@ const MAP_KINGDOM = {
   row: 2,
 
   x: 200,
-  y: 770,
+  y: 790,
 
   width: 258,
   height: 205
